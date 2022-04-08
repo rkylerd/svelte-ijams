f7ebb72cb564931d7a2378f7b020255e0395530d
Kyler <47895580+rkylerd@users.noreply.github.com>
2022-04-07 19:30:49 -0600

    Making api calls work locally on in prod

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,13 @@
+const host = `${window.location.protocol}//${window.location.host}`;
 export const getArtistAlbums = async (artistId: string) => {
-    const res = await fetch(`api/artistalbums/${artistId}`);
+    const res = await fetch(`${host}/api/artistalbums/${artistId}`);
     const { results = [] } = await res.json();
 
     return results.filter(({ wrapperType }) => wrapperType === "collection");
 };
 
 export const getAlbum = async (albumId: string | number) => {
-    const res = await fetch(`api/album/${albumId}`);
+    const res = await fetch(`${host}/api/album/${albumId}`);
     const { results } = await res.json();
     const indexOfAlbumInfo = results.findIndex(({ wrapperType }) => wrapperType !== "track")
     const album = {
